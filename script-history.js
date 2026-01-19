aa691fb967f24eb5ca1b9c251382a2bf0994f118
a.mizerny <a.mizerny@kryptonite.ru>
2026-01-20 01:29:39 +0300

    history fixes

--- script-history.js
+++ script-history.js
@@ -1,8 +1,11 @@
 $(function () {
     /*СЛАЙДЕР ИСТОРИИ*/
-    const swiperEl = document.querySelector(".swiper-history");
+
+    const swiperEl = document.querySelector(".scroll-anchor");
     const footerEl = document.querySelector("footer");
 
+    // Якорь для скролла
+    gsap.registerPlugin(ScrollTrigger);
     const swiperScrollTrigger = ScrollTrigger.create({
         trigger: swiperEl,
         pin: true,
@@ -12,6 +15,7 @@ $(function () {
         end: "top bottom",
     });
 
+    // Слайдер
     let swiperHistory = new Swiper(".swiper-history", {
         effect: "coverflow",
         grabCursor: true,
@@ -25,12 +29,7 @@ $(function () {
             modifier: 1,
             slideShadows: false,
         },
-        pagination: {
-            el: ".swiper-history-pagination",
-            clickable: true,
-        },
     });
-
     const toggleText = (index) => {
         const tesxtEls = document.querySelectorAll('.history-slider-text');
         tesxtEls.forEach(el => {
@@ -43,13 +42,68 @@ $(function () {
         
         swiperScrollTrigger.refresh();
     }
-
     swiperHistory.on('init', function(event) {
         toggleText(event.activeIndex);
     });
-    swiperHistory.on('slideChange', (event) => {
-        toggleText(event.activeIndex);
+    swiperHistory.on('slideChange', (swiper) => {
+        if (activeBullet) {
+            activeBullet.classList.remove('active');
+        }
+        activeBullet = swiperHistoryPagination.slides[swiper.activeIndex];;
+        activeBullet.classList.add('active');
+
+        toggleText(swiper.activeIndex);
+    });
+    swiperHistory.on('click', (swiper, event) => {
+        const { clientX, clientY } = event;
+
+        const slides = swiper.slides;
+        let targetIndex = null;
+
+        slides.forEach((slide, index) => {
+            const rect = slide.getBoundingClientRect();
+
+            if (
+                clientX >= rect.left &&
+                clientX <= rect.right &&
+                clientY >= rect.top &&
+                clientY <= rect.bottom
+            ) {
+                targetIndex = index;
+            }
+        });
+
+        if (targetIndex != null) {
+            swiper.slideTo(targetIndex);
+        }
     });
     swiperHistory.init();
+
+    // Пагинация
+    let swiperHistoryPagination = new Swiper(".swiper-history-pagination", {
+        slidesPerView: 'auto',
+        freeMode: true,
+        mousewheel: true,
+        init: false,
+    });
+    let activeBullet;
+    swiperHistoryPagination.on('init', (swiper) => {
+       if (activeBullet) {
+            activeBullet.classList.remove('active');
+        }
+        activeBullet = swiper.slides[0];
+        activeBullet.classList.add('active');
+        swiperHistory.slideTo(swiper.clickedIndex);
+    });
+    swiperHistoryPagination.on('click', (swiper) => {
+        if (activeBullet) {
+            activeBullet.classList.remove('active');
+        }
+        activeBullet = swiper.clickedSlide;
+        activeBullet.classList.add('active');
+        swiperHistory.slideTo(swiper.clickedIndex);
+    });
+    swiperHistoryPagination.init();
+
     /*END СЛАЙДЕР ИСТОРИИ*/
 })
